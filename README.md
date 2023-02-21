95df517100172d38d651b6d23989a4f2a5c4f018
Mia Borja <apk.borja@gmail.com>
2023-02-20 19:52:41 -0800

    added comments

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Web Scraper
 
-A web scraper for pulling figure entries.
+A scraper for pulling entries from MFC. Includes name and link to the entry.
+
+All comments for changing parameters are located in the file.
+
+Will continue to update, but at the moment this is just a way to learn.
+
+## How to
+- Run the `scraper.py` file
--- f-scraper/scraper.py
+++ f-scraper/scraper.py
@@ -2,19 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# Sample URL for testing
+# Change URL to desired Search URL. Remove the page number at the end.
 url_to = 'https://myfigurecollection.net/browse.v4.php?mode=search&rootId=0&status=-1&categoryId=-1&orEntries[]=1590&domainId=-1&noReleaseDate=0&releaseTypeId=0&ratingId=0&isCastoff=0&hasBootleg=0&tagId=0&noBarcode=0&clubId=0&isDraft=0&year=2023&month=2&acc=0&separator=0&sort=insert&output=2&current=categoryId&order=desc&page='
-
-"""
-TO DO
-
-Format the data.
-"""
 
 figure_entries = []
 
-# Maybe add an extra method to get the official images from the figure entry as well sometime.
+# Working on adding a method to pull the first img from the entry pages. 
 
+
+# Will update this, but change the second parameter to the maximum amount of pages.
 for pages in range(1, 20):
   resp = requests.get(url_to + str(pages))
   soup = BeautifulSoup(resp.content, 'html.parser')
